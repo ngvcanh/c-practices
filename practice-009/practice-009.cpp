0f4abed09532c8fbb873cc3a18c86aeeac5f0547
ngvcanh <ngvcanh2014@gmail.com>
2024-08-10 18:28:49 +0700

    Practice 009: T(n) = 1 x 2 x 3 x ... x n

--- practice-009/practice-009.cpp
+++ practice-009/practice-009.cpp
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
 /**
- * Calculate S(n) = 1 x 2 x 3 x ... x n
+ * Calculate T(n) = 1 x 2 x 3 x ... x n
  * Only using while loop.
  */
 int main() {
-	int s = 0;
+	int t = 0;
 	int n;
 
 	printf("Enter value of n:");
 	scanf_s("%d", &n);
 
 	if (n > 0) {
-		s = 1;
+		t = 1;
 	}
 
 	while (n > 0) {
-		printf("x %d\n", n);
-		s *= n;
+		t *= n;
 		--n;
 	}
 
-	printf("S(n) = %d", s);
+	printf("T(n) = %d", t);
 
 	return 0;
 }
