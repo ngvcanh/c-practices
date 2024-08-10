023fca6237adf3f007013117afe4bc9ee1e110eb
ngvcanh <ngvcanh2014@gmail.com>
2024-08-11 00:31:10 +0700

    Practice 011: S(n) = 1 + 1.2 + 1.2.3 + ... + 1.2.3...n

--- practice-011/practice-011.cpp
+++ practice-011/practice-011.cpp
@@ -28,3 +28,30 @@ int main() {
 
 	return 0;
 }
+
+/**
+ * Because the term at the (n - 1)th position of the sum is multiplied by n 
+ * at the nth position in the sum, we can approach the solution as follows: 
+ */
+ /*
+int main() {
+	long s = 0;
+	int n;
+
+	printf("Enter value of n:");
+	scanf_s("%d", &n);
+
+	long t = 1;
+	int i = 1;
+
+	while (i <= n) {
+		t *= i;
+		s += t;
+		++i;
+	}
+
+	printf("S(n) = %ld", s);
+
+	return 0;
+}
+*/
