db564a0213354988a829436646c9a76d5cd74ce9
ngvcanh <ngvcanh2014@gmail.com>
2024-08-24 15:14:58 +0700

    Practice 033: Calculate S(n) = sqrt(2 + sqrt(2 + ... + sqrt(2 + sqrt(2)))) with n square root signs
    Practice 033: Calculate S(n) = sqrt(2 + sqrt(2 + ... + sqrt(2 + sqrt(2)))) with n square root signs

--- practice-033/practice-033.cpp
+++ practice-033/practice-033.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 
 /**
- * Calculate S(n) = sqrt(2 + sqrt(2 + …. + sqrt(2 + sqrt(2)))) with n square root signs.
+ * Calculate S(n) = sqrt(2 + sqrt(2 + ... + sqrt(2 + sqrt(2)))) with n square root signs.
  * Only using while loop.
  */
 int main() {
