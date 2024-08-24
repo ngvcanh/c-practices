b756ce0e61b19e9e482e58051623ea29d7fae763
ngvcanh <ngvcanh2014@gmail.com>
2024-08-24 15:13:25 +0700

    Practice 033: Calculate S(n) = sqrt(2 + sqrt(2 + �. + sqrt(2 + sqrt(2)))) with n square root signs
    Practice 033: Calculate S(n) = sqrt(2 + sqrt(2 + �. + sqrt(2 + sqrt(2)))) with n square root signs

---
+++ practice-033/README.md
@@ -0,0 +1,5 @@
+# Practice 033: Calculate S(n) = sqrt(2 + sqrt(2 + …. + sqrt(2 + sqrt(2)))) with n square root signs
+
+Calculate S(n) = sqrt(2 + sqrt(2 + …. + sqrt(2 + sqrt(2)))) with n square root signs.
+
+Only using while loop.
---
+++ practice-033/practice-033.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * Calculate S(n) = sqrt(2 + sqrt(2 + …. + sqrt(2 + sqrt(2)))) with n square root signs.
+ * Only using while loop.
+ */
+int main() {
+	int n;
+
+	printf("Enter value of n: ");
+	scanf_s("%d", &n);
+
+	if (n <= 0) {
+		printf("S(n) = 0");
+		return 0;
+	}
+
+	double s = sqrt(2);
+
+	while (n > 1) {
+		s = sqrt(2 + s);
+		--n;
+	}
+
+	printf("S(n) = %.4f", s);
+
+	return 0;
+}
